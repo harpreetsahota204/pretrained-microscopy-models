47cacb1992c1b3c04528f8a3ed0aeffc345341ee
Harpreet Sahota <sahota.singh.harpreet@gmail.com>
2025-09-21 15:50:48 -0500

    update init

--- __init__.py
+++ __init__.py
@@ -8,5 +8,18 @@ def download_model(model_name, model_path, **kwargs):
 
 
 def load_model(model_name, model_path, **kwargs):
-    """Loads the model."""
+    """Loads the model.
+    
+    Args:
+        model_name: the name of the model to load
+        model_path: the path to the model directory
+        **kwargs: additional arguments including:
+            - classes: number of segmentation classes (default: 4)
+            - architecture: model architecture (default: 'UnetPlusPlus')
+            - encoder: encoder backbone (default: 'resnet50')
+            - encoder_weights: pretrained weights (default: 'micronet')
+    
+    Returns:
+        a Pretrained_Microscopy_Model instance
+    """
     return Pretrained_Microscopy_Model(model_path=model_path, **kwargs)
